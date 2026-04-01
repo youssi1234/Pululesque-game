786720a955b239f8d14b08c1a8a40f55f51438b1
Youssi <andrianoayoussi@gmail.com>
2026-04-01 15:04:17 +0200

    correction quit

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ screen = pygame.display.set_mode((800, 600))
 running = True
 while running: 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             running = False
 pygame.quit()
