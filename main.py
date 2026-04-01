8044f290ea201190ba9e5d8d54a1a8ee366d5c34
Youssi <andrianoayoussi@gmail.com>
2026-04-01 15:08:17 +0200

    change

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((800, 600))
 running = True
 while running: 
     for event in pygame.event.get():
+        screen.fill((0,0,0))
+        pygame.display.flip()
         if event.type == pygame.QUIT:
             running = False
 pygame.quit()
