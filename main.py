7970a8a72c0c978c8f31e7bcb2badb8febde8897
Youssi <andrianoayoussi@gmail.com>
2026-04-03 07:39:40 +0200

    button + bg

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pygame
 pygame.init()
 pygame.display.set_caption("Pygame Window")
-pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((600, 400))
+
+background_original = pygame.image.load("sky.jpg").convert()
+background = pygame.transform.scale(background_original, (600, 400))
 
 running = True
 while running: 
     for event in pygame.event.get():
-        screen.fill((0,0,0))
+        screen.blit(background, (0, 0))
+        button = pygame.draw.rect(screen, (255, 0, 0), (230, 50, 100, 80))
+
+        if button.collidepoint(pygame.mouse.get_pos()):
+            if pygame.mouse.get_pressed()[0]:
+                print("Button clicked!")
+        
         pygame.display.flip()
         if event.type == pygame.QUIT:
             running = False
