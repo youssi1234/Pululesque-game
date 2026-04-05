9e334ab2fdf8c9c42846fe0a1a7193a62fe7f754
Youssi <andrianoayoussi@gmail.com>
2026-04-06 01:42:30 +0200

    fond changer en plus beau quoi

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ class Game:
     def __init__(self,etat, screen_width , screen_height):
         self.etat = etat
         self.button = Button(50 , 50 , 30 , 30 , (255, 255, 255) , "<-")
-        bg = pygame.image.load("assets/sunset_clouds_over_the_sea_pixel_background/4.png").convert()
+        bg = pygame.image.load("assets/fond/10.png").convert()
         self.bg = pygame.transform.scale(bg,(screen_width,screen_height))
 
     def draw(self,screen):
