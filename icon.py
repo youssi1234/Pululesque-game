b37dfca00181a0c32163237b951cf7972b6d212b
Youssi <andrianoayoussi@gmail.com>
2026-04-06 06:23:45 +0200

    mhh

--- icon.py
+++ icon.py
@@ -11,12 +11,10 @@ class Icon(pygame.sprite.Sprite):
         self.images_idle_back = []
         self.last_direction = "right"  # Par défaut, le personnage regarde vers la droite
 
-        # Chargement course (RUSH)
         for i in range(6):
             brute = pygame.image.load(f"assets/craftpix-net-622999-free-pixel-art-tiny-hero-sprites/rush-pink/{i+1}.PNG").convert_alpha()
             self.images_run.append(pygame.transform.scale(brute, (70, 70)))
             
-        # Chargement repos (IDLE) - Ajuste le chemin vers ton image fixe
         for i in range(4):
             brute = pygame.image.load(f"assets/craftpix-net-622999-free-pixel-art-tiny-hero-sprites/idle-pink/{i+1}.PNG").convert_alpha()
             self.images_idle.append(pygame.transform.scale(brute, (70, 70)))
@@ -24,12 +22,10 @@ class Icon(pygame.sprite.Sprite):
         for i in range(6):
             self.images_run_back.append(pygame.transform.flip(self.images_run[i], True, False))
             
-        # Chargement repos (IDLE) - Ajuste le chemin vers ton image fixe
         for i in range(4):
             self.images_idle_back.append(pygame.transform.flip(self.images_idle[i], True, False))
 
         self.current_images = self.images_idle
-
 
         self.rect = self.images_run[0].get_rect()
         self.rect.x = (screen.get_width() / 2) - (self.rect.width / 2)
@@ -49,21 +45,6 @@ class Icon(pygame.sprite.Sprite):
         is_moving_down = self.key_Held.get(pygame.K_DOWN)
         is_moving_left = self.key_Held.get(pygame.K_LEFT)
         is_moving_right = self.key_Held.get(pygame.K_RIGHT)
-
-        # verification an'ilay evenement mietsika 
-        # if is_moving:
-        #     if is_moving_right or is_moving_up or is_moving_down:
-        #         self.current_images = self.images_run
-        #         self.last_direction = "right"
-        #     elif is_moving_left:
-        #         self.current_images = self.images_run_back
-        #         self.last_direction = "left"
-
-        # # ito ndray ho an'ilay tsy mietsika izay miverina amin'ilay idle
-        # elif not is_moving and self.last_direction == "right":
-        #     self.current_images = self.images_idle
-        # elif not is_moving and self.last_direction == "left":
-        #     self.current_images = self.images_idle_back
 
         if is_moving_right:
             self.last_direction = "right"
@@ -86,7 +67,6 @@ class Icon(pygame.sprite.Sprite):
         if self.key_Held.get(pygame.K_RIGHT):
             if self.rect.x >= (screen.get_width() - self.rect.width) - utiles.marge_X(screen, 10):
                 self.rect.x = (screen.get_width() - self.rect.width) - utiles.marge_X(screen, 10)
-                
             else:
                 self.rect.x += self.velocity
 
@@ -107,8 +87,6 @@ class Icon(pygame.sprite.Sprite):
                 self.rect.y = (screen.get_height() - self.rect.height) - utiles.marge_Y(screen, 10)
             else:
                 self.rect.y += self.velocity
-
-       
 
     def move(self, screen):
         self.direction()
