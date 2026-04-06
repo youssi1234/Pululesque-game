f64bb0fa3179ef26afa7ae75ecb22e8fa77faff6
Youssi <andrianoayoussi@gmail.com>
2026-04-06 04:21:29 +0200

    suppr

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from game import Game
 from etat import Etat
 from loadingAnim import LoadingAnim
 
-
 pygame.init()
 
 screen_width = 800
@@ -17,16 +16,19 @@ screen = pygame.display.set_mode(size_screen)
 
 game_state = Etat()
 menu = Menu(game_state , screen_width, screen_height)
-game = Game(game_state , screen_width , screen_height)
+game = Game(game_state , screen_width , screen_height , screen)
 loadingAnim = LoadingAnim()
+
 
 fluidity = 60
 clock = pygame.time.Clock()
 
 running = True
 while running:
-    # 1. GESTION DES ÉVÉNEMENTS (Clavier/Souris)
-    for event in pygame.event.get():
+
+    events = pygame.event.get()
+
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
 
@@ -40,8 +42,9 @@ while running:
         loadingAnim.draw(screen)
 
     elif game_state.current_state == "game":
+        game.handle_events(events)
         game.draw(screen)
-    
+        game.icon_player.move(screen)
 
 
     pygame.display.flip()
--- perso.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from icon import Icon
-
-class Perso(pygame.sprite.Sprite):
-    def __init__(self, screen):
-        self.icon_player = Icon(screen)
